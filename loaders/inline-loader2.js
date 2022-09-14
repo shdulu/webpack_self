4cf2d7ccf538bf24a02383a397f3ecfa803eb327
shdulu <17625329975>
2022-09-14 22:00:44 +0800

    update

--- loaders/inline-loader2.js
+++ loaders/inline-loader2.js
@@ -5,6 +5,6 @@ function loader(source) {
 loader.pitch = function() {
   console.log('inline2-pitch');
   // pitch 有返回值的话 会跳国后面的loader执行，执行前一个loader的normal函数
-  return 'inline2-result'
+  // return 'inline2-result'
 }
 module.exports = loader;
--- loaders/post-loader2.js
+++ loaders/post-loader2.js
@@ -1,8 +1,12 @@
 function loader(source) {
   console.log("post2");
-  return source + "//post2";
+  // 如果让loader变成异步: 调用this.async 方法可以把 loader的执行从同步变成异步
+  let callback = this.async();
+  setTimeout(() => {
+    callback(null, source + "//post2");
+  }, 3000);
 }
-loader.pitch = function() {
-  console.log('post2-pitch');
-}
+loader.pitch = function () {
+  console.log("post2-pitch");
+};
 module.exports = loader;
